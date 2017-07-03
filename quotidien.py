cdf5c7ee52828c3abd1ca92deadb73a7d3830feb
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2017-07-03 15:17:46 +0100

    init

---
+++ quotidien.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import redis
+import datetimeimport 
+import os
+
+app = Flask(__name__)
+r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ["REDIS_PORT"], db=1)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/reg', methods=['POST'])
+def register():
+    name = request.form['name']
+    date = datetime.datetime.now()
+
+    useragent = request.headers.get('User-Agent')
+
+    r.set((name, date), useragent)
+    return render_template('done.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
